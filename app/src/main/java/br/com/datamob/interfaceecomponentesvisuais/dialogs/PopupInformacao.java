21d7bd97f13c660fd395b99372dfde809b06fde8
Rafael Sarreta Lucindo <rafael@datamob.com.br>
2018-09-01 12:34:49 -0300

    Parcial

---
+++ app/src/main/java/br/com/datamob/interfaceecomponentesvisuais/dialogs/PopupInformacao.java
@@ -0,0 +1,30 @@
+package br.com.datamob.interfaceecomponentesvisuais.dialogs;
+
+import android.content.Context;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class PopupInformacao
+{
+    public static void mostraMensagem(final Context context, Handler handler,final String mensagem)
+    {
+        handler.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                mostraMensagem(context, mensagem);
+            }
+        });
+    }
+
+    public static void mostraMensagem(Context context, String mensagem)
+    {
+        Toast.makeText(context, mensagem, Toast.LENGTH_LONG).show();
+    }
+
+    public static void mostraMensagem(Context context, int mensagem)
+    {
+        Toast.makeText(context, mensagem, Toast.LENGTH_LONG).show();
+    }
+}
